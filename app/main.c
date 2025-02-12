6ccdd2a7ac0e5b2df0ff2b20c3ac121a864fa808
melmon <dgd@melmon.dev>
2025-02-12 20:38:46 +0000

    echo

--- app/main.c
+++ app/main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+  char input[100];
 
   while (1) {
     printf("$ ");
-    // Wait for user input
-    char input[100];
+    fflush(stdout);
     fgets(input, 100, stdin);
+
+    if (strlen(input) == 1)
+      continue;
 
     input[strlen(input) - 1] = '\0';
 
-    if (!strcmp(input, "exit 0")) {
-      break;
+    char *command = strdup(input);
+    command = strtok(command, " ");
+
+    if (!strcmp(command, "exit")) {
+      return 0;
+    } else if (!strcmp(command, "echo")) {
+      if (strlen(input) > 5)
+        printf("%s\n", input + 5);
+    } else {
+      printf("%s: command not found\n", input);
     }
 
-    printf("%s: command not found\n", input);
+    free(command);
   }
-  return 0;
+  return 1;
 }
