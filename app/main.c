a642a6bdb1bdbd2003ea0d05d581c65890b7d99d
melmon <dgd@melmon.dev>
2025-02-12 20:06:16 +0000

    builtin stuff

--- app/main.c
+++ app/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
+  while (1) {
+    printf("$ ");
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    input[strlen(input) - 1] = '\0';
+
+    if (!strcmp(input, "exit 0")) {
+      break;
+    }
+
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
