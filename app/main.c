ba82dad5318dc199e4aaf41260564f8840f72b7d
melmon <dgd@melmon.dev>
2025-02-12 20:10:53 +0000

    repl

--- app/main.c
+++ app/main.c
@@ -5,15 +5,14 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
+  while (1) {
+    printf("$ ");
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
-
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
