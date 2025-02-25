8c9571e1bde05341c315b0d70e543999487ee201
melmon <dgd@melmon.dev>
2025-02-25 23:02:00 +0000

    update to make tests happy

--- app/run.c
+++ app/run.c
@@ -71,8 +71,12 @@ int run_path_binary(const int argc, char *argv[]) {
     char full_path[300];
     snprintf(full_path, sizeof(full_path), "%s/%s", directory_buffer, argv[0]);
     int status_code = execvp(full_path, argv);
-    printf("/!\\ If you can see this, something has gone wrong! /!\\\n");
-    printf("Status code: %d\n", status_code);
+    printf("Program was passed %d args (including program name)", argc);
+    printf("Arg #0 (program name): %s", argv[0]);
+    for (int i = 1; i < argc; i++) {
+      printf("Arg #%d: %s", i, argv[i]);
+    }
+    printf("Program Signature: %d", child_pid);
     return -1;
   }
   // we are the parent
